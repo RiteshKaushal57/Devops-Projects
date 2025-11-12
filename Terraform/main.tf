91da42a6027e3d7dc7c1117634a4503802cd3a5d
Ritesh Kaushal <riteshkaushal57@gmail.com>
2025-11-12 11:56:20 +0530

    c

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,8 @@
 resource "aws_vpc" "myvpc" {
-  cidr_block = "
-  
+  cidr_block = var.cidr_block
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,3 @@
-variable "" {
-  
+variable "cidr_block" {
+  default = "10.0.0.0/16"
 }
